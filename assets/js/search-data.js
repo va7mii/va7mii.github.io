115e7ebbcfea6d0cf2a8e4f6b206c7387eba3d98
va7mii <85780191+va7mii@users.noreply.github.com>
2025-01-10 00:02:05 +0000

    Deploying to gh-pages from @ va7mii/va7mii.github.io@efb3f06007edd6032361f0a2883f086698ab6f21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,18 +72,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-formatting-and-links",
-      
-        title: "a post with formatting and links",
-      
-      description: "a test post",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2025/test/";
-        
-      },
-    },{id: "post-sup",
+            },{id: "post-sup",
       
         title: "Sup",
       
@@ -92,17 +81,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2024/sup/";
-        
-      },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
         
       },
     },{id: "post-snowboarding",
@@ -127,36 +105,17 @@ ninja.data = [{
           window.location.href = "/blog/2023/belgium/";
         
       },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
     },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-3d-quot-touchable-quot-augmented-reality-cubes-using-a-depth-camera",
+          title: '3D &amp;quot;Touchable&amp;quot; Augmented-Reality Cubes using a Depth Camera',
+          description: "Playing with OpenCV and Intel Realsense camera modules in Python.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-line-following-robot-with-bluetooth-control",
+          title: 'Line-Following Robot with Bluetooth Control',
+          description: "An IGEN 230 project that makes use of PID control, Bluetooth, and ESP-32&#39;s FreeRTOS to handle multithreading.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{
